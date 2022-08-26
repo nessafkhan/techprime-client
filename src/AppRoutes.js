78b262aad028d451933ea2c4c54c6f6f4ff20679
nessaf khan <nessafkhan.kn@gmail.com>
2022-08-26 21:21:36 +0530

    login bug fixed

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,23 +1,29 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Routes,useNavigate } from 'react-router-dom';
 import AddProjectPage from './Pages/AddProjectPage';
 import Dashboard from './Pages/Dashboard';
 import ListProjectPage from './Pages/ListProjectPage';
 import LoginPage from './Pages/LoginPage';
 import { useSelector } from 'react-redux';
+
 const AppRoutes = () => {
 	const user = useSelector((state) => state.user.authenticated);
+	const navigate = useNavigate();
+
+	useEffect(()=>{
+		if(!user){
+			navigate('/')
+		}else{
+			navigate('/dashboard');
+		}
+	}, [user]);
 	return (
 		<Routes>
 			<Route path="/" element={<LoginPage />} />
-			{user && (
-				<>
 					<Route path="/dashboard" element={<Dashboard />} />
 					<Route path="/add-project" element={<AddProjectPage />} />
 					<Route path="/project" element={<ListProjectPage />} />
 					<Route path="*" element={<h1>404 Not found</h1>} />
-				</>
-			)}
 		</Routes>
 	);
 };
